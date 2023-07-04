d24cdc072c08511804fdf981addfcdd9e71573d8
David Flores <dcflores@live.com>
2023-07-04 14:43:20 -0700

    modified routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ module.exports = {
   // Get all users
   async getUsers(req, res) {
     try {
-      const users = await User.find();
+      const users = await User.find().populate('friends');
       res.json(users);
     } catch (err) {
       res.status(500).json(err);
@@ -14,7 +14,8 @@ module.exports = {
   async getSingleUser(req, res) {
     try {
       const user = await User.findOne({ _id: req.params.userId })
-        .select('-__v');
+       .populate('friends')  
+       .select('-__v');
 
       if (!user) {
         return res.status(404).json({ message: 'No user with that ID' });
@@ -45,6 +46,40 @@ module.exports = {
 
       await Application.deleteMany({ _id: { $in: user.applications } });
       res.json({ message: 'User and associated apps deleted!' })
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId }, // find a document with this id
+        req.body, // document to insert when nothing was found
+        {new: true, runValidators: true} // options: return updated one, validate before update
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async addFriend(req, res) {
+    try {
+      const user = await User.findByIdAndUpdate(
+        req.params.userId, // The id of the user to whom the friend will be added.
+        { $addToSet: { friends: req.body.friendId } }, // The id of the friend to add.
+        { new: true, runValidators: true } // Options: return the modified document rather than the original. runValidators ensures new friendId is a valid ObjectId.
+      );
+  
+      if (!user) {
+        return res.status(404).json({ message: 'No user with this ID' });
+      }
+  
+      res.json(user);
     } catch (err) {
       res.status(500).json(err);
     }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,13 +4,34 @@ const {
   getSingleUser,
   createUser,
   deleteUser,
+  updateUser,
+  addFriend,
 } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser);
-router.route('/:userId').get(getSingleUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+
+// /api/users/:userId/friends
+router.route('/:userId/friends').post(addFriend);
 
 module.exports = router;
+// const router = require('express').Router();
+// const {
+//   getUsers,
+//   getSingleUser,
+//   createUser,
+//   deleteUser,
+//   updateUser,
+// } = require('../../controllers/userController');
+
+// // /api/users
+// router.route('/').get(getUsers).post(createUser);
+
+// // /api/users/:userId
+// router.route('/:userId').get(getSingleUser);
+// router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+
+// module.exports = router;
